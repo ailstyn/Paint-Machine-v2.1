bd792eed56770317d483625f476f79d4fd6570c6
dan <theredcrosss@gmail.com>
2025-06-17 11:04:38 -0400

    lksdjf

--- sensor-relay-project/arduino/scale_controller/scale_controller.ino
+++ sensor-relay-project/arduino/scale_controller/scale_controller.ino
@@ -59,10 +59,10 @@ void handshake_station_id() {
         while (Serial.available() > 0) {
             byte cmd = Serial.read();
             if (cmd == GET_ID) {
-                delay(200);
+                delay(500);
                 Serial.println("<ID:" + String(STATION_ID) + ">");
                 digitalWrite(LED_PIN, LOW); // Turn off LED after handshake
-                delay(200);
+                delay(500);
                 return;
             }
         }
