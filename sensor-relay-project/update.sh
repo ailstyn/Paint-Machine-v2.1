71f801ae72ac1114dfb3dfaffec6d143297c2760
dan <theredcrosss@gmail.com>
2025-08-20 12:03:14 -0400

    u

--- sensor-relay-project/update.sh
+++ sensor-relay-project/update.sh
@@ -7,17 +7,14 @@ set -e
 echo "Updating repository..."
 sudo git pull
 
-# 2. Copy kiosk startup script to home directory
-cp start_kiosk.sh /home/chris/
-
-# 3. Compile the Arduino sketch
+# 2. Compile the Arduino sketch
 SKETCH_PATH="arduino/scale_controller/scale_controller.ino"
 FQBN="arduino:avr:leonardo"  # Change this if you use a different board type
 
 echo "Compiling $SKETCH_PATH ..."
 arduino-cli compile --fqbn $FQBN $SKETCH_PATH
 
-# 4. Find connected Arduinos
+# 3. Find connected Arduinos
 echo "Detecting connected Arduinos..."
 PORTS=$(ls /dev/ttyACM* /dev/ttyUSB* 2>/dev/null || true)
 
@@ -26,7 +23,7 @@ if [ -z "$PORTS" ]; then
     exit 1
 fi
 
-# 5. Upload to each Arduino
+# 4. Upload to each Arduino
 for PORT in $PORTS; do
     echo "Uploading to $PORT ..."
     arduino-cli upload -p $PORT --fqbn $FQBN $SKETCH_PATH
