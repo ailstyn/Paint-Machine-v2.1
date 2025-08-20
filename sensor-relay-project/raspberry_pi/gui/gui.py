2ec28aa22bf1a80e586af3f81d138cbebd37daef
dan <theredcrosss@gmail.com>
2025-08-20 15:00:47 -0400

    x

--- sensor-relay-project/raspberry_pi/gui/gui.py
+++ sensor-relay-project/raspberry_pi/gui/gui.py
@@ -434,9 +434,9 @@ class StationWidget(QWidget):
         # Large weight label
         self.weight_label = OutlinedLabel("0 / 0 g", parent=self)
         self.weight_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.weight_label.setFont(QFont("Arial", 76, QFont.Weight.Bold))  # Large font for weight display
-        self.weight_label.setStyleSheet("font-size: 76pt; color: #fff;")
-        self.weight_label.setFixedHeight(110)  # Fixed height for consistent font rendering
+        self.weight_label.setFont(QFont("Arial", 54, QFont.Weight.Bold))  # Large font for weight display
+        self.weight_label.setStyleSheet("font-size: 54pt; color: #fff;")
+        self.weight_label.setFixedHeight(80)  # Fixed height for consistent font rendering
         content_layout.addWidget(self.weight_label, stretch=0)
 
         # Status label
